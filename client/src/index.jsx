4375af3e96a0146949e43207b80740da56549b5f
Velicko00 <145012953+andjelabel@users.noreply.github.com>
2024-02-07 18:33:40 +0100

    authContext

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
         <App />
       </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )

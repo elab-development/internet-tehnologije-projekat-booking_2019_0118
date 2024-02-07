6a0fbdabc69b52746d102989c191857198574e67
Velicko00 <145012953+andjelabel@users.noreply.github.com>
2024-02-07 22:11:38 +0100

    login css

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -12,6 +12,7 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState("");
@@ -31,6 +32,7 @@ const Header = ({ type }) => {
   });
 
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -69,7 +71,7 @@ const Header = ({ type }) => {
             <p className="headerDesc">
               Search low prices on hotels and much more!
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {!user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
